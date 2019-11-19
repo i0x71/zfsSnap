641ddd33fbaca56516a1f702eaca2c8e0d915100
i0x71 <49561181+i0x71@users.noreply.github.com>
2019-11-19 11:26:30 -0500

    Update zfsSnap.sh

--- zfsSnap.sh
+++ zfsSnap.sh
@@ -11,7 +11,8 @@ olderThan=$2
 
 while read -r snapshot
 do
-  snapshotDate=`echo $snapshot | awk -F '@' '{ print $2 }' | awk -F '_' '{ print $2 }'`
+  snapshotDate=`echo $snapshot | awk -F '@' '{ print $2 }' | awk -F '_' '{ print $NF }'`
+echo $snapshotDate
   let snapshotAge=dateNow-snapshotDate
   echo "[+] $snapshot|$snapshotAge"
   if (( $snapshotAge > $olderThan ))
@@ -21,7 +22,7 @@ do
     if test $status -eq 0
     then
       echo "[+] Destroying $snapshot"
-      /usr/local/sbin/zfs destroy $snapshot
+#      /usr/local/sbin/zfs destroy $snapshot
     fi
   fi
 done < <(zfs list -t snapshot -H | awk '{ print $1 }' | grep $snapshotName)
